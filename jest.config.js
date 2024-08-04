25a084506cb1ce6338e61f91161047ad82b06d06
bfx17 <bfx17@albaik.com.sa>
2024-08-04 13:04:02 +0300

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,21 @@
 module.exports = {
-  preset: 'react-native',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  testMatch: [
-    '**/__tests__/**/*.ts?(x)',
-    '**/?(*.)+(spec|test).ts?(x)',
+  // transform: {
+  //   '^.+\\.(ts|tsx)$': 'ts-jest',
+  //   '^.+\\.(js|jsx)$': 'babel-jest',
+  // },
+  // testMatch: [
+  //   '**/__tests__/**/*.ts?(x)',
+  //   '**/?(*.)+(spec|test).ts?(x)',
+  // ],
+  setupFilesAfterEnv: [
+    './jest.setup.js',
+    '@testing-library/jest-native/extend-expect',
   ],
+  transformIgnorePatterns: [
+    'node_modules/(?!@react-native|react-native)(?!@react-navigation|react-navigation)(?!react-redux)',
+  ],
+  preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['@testing-library/react-native/cleanup-after-each'],
-  setupFilesAfterEnv: ['./jest.setup.ts'],
+  // setupFilesAfterEnv: ['@testing-library/react-native/cleanup-after-each'],
+
 };
